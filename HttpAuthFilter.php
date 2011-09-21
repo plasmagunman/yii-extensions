61be1d798f825c6747089e9d9159a9a59f1e7a36
plasmagunman <plasmagunman@users.sourceforge.net>
2011-09-22 00:03:47 +0200

    cleaned up whitespaces, yii uses tabs, so do i.

--- HttpAuthFilter.php
+++ HttpAuthFilter.php
@@ -9,24 +9,24 @@
  * HttpAuthFilter performs authorization checks using http authentication
  *
  * By enabling this filter, controller actions can be limited to a couple of users.
- * It is very simple, supply a list of usernames and passwords and the controller actions 
+ * It is very simple, supply a list of usernames and passwords and the controller actions
  * will be restricted to only those. Nothing fancy, it just keeps out users.
- * 
+ *
  * To specify the authorized users specify the 'users' property of the filter
  * Example:
  * <pre>
  *
- *	public function filters()
- *	{
- *		return array(
- *           array(
- *			'HttpAuthFilter',
- *                'users'=>array('admin'=>'admin'), 
- *                'realm'=>'Admin section'
- *                  )  
- *            );
- *	}
- * The default section for the users property is 'admin'=>'admin' change it
+ * public function filters()
+ * {
+ *     return array(
+ *         array(
+ *             'HttpAuthFilter',
+ *             'users'=>array('admin'=>'admin'),
+ *             'realm'=>'Admin section'
+ *         )
+ *     );
+ * }
+ * The default section for the users property is 'admin'=>'admin'. Change it!
  *
  */
 class HttpAuthFilter extends CFilter
@@ -39,7 +39,7 @@ class HttpAuthFilter extends CFilter
 	/**
 	 * @return string authentication realm
 	 */
-    public $realm='Authentication needed'; 
+	public $realm='Authentication needed';
 
 	/**
 	 * Performs the pre-action filtering.
@@ -49,19 +49,18 @@ class HttpAuthFilter extends CFilter
 	 */
 	protected function preFilter($filterChain)
 	{
-        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) 
-        {
-            $username=$_SERVER['PHP_AUTH_USER'];
-            $password=$_SERVER['PHP_AUTH_PW'];
-            
-            if($this->users[$username]==$password)
-            {
-                return true;
-            }                
+		if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
+		{
+			$username=$_SERVER['PHP_AUTH_USER'];
+			$password=$_SERVER['PHP_AUTH_PW'];
 
-        }
-        header("WWW-Authenticate: Basic realm=\"".$this->realm."\"");  
-        throw new CHttpException(401,Yii::t('yii','You are not authorized to perform this action.'));
+			if($this->users[$username]==$password)
+			{
+				return true;
+			}
+
+		}
+		header("WWW-Authenticate: Basic realm=\"".$this->realm."\"");
+		throw new CHttpException(401,Yii::t('yii','You are not authorized to perform this action.'));
 	}
 }
-
